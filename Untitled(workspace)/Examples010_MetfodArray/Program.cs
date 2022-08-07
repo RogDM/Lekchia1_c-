5bb66fe561c1e7a582c8e3e7ede62b8fee762482
Dmitry Mikhailov <arlit112@gmail.com>
2022-08-07 14:14:54 +0300

    add arrey

---
+++ Untitled(workspace)/Examples010_MetfodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 15, 11, 22, 33, 44, 11, 2, 5 };
+
+int n = array.Length;
+int find = 4;
+
+int index =0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
