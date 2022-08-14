9df464dad51ce60c791efce7444db3f26b132609
Dmitry Mikhailov <arlit112@gmail.com>
2022-08-14 21:23:53 +0300

    add example12_for_for

--- Untitled(workspace)/Example012_For/Program.cs
+++ Untitled(workspace)/Example012_For/Program.cs
@@ -1,5 +1,5 @@
 ﻿// for цикл (по итогу метода 4)
-string Method4(int count, string text)
+/* string Method4(int count, string text)
 {
     string result = String.Empty;
 
@@ -21,5 +21,76 @@ for (int i = 2; i <= 10; i++)
         Console.WriteLine($"{i} x {j} = {i * j}");
     }
     Console.WriteLine();
+} */
+
+// Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// Маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = “qwerty”
+//             012345
+// s[3] // r
+/* string Replase(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
 }
+string newText = Replase(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+string newText2 = Replase(text, 'к', 'К');
+Console.WriteLine(newText); */
+
+
+// Сортиврока числа от меньшего к большиму
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PtintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+
+    }
+
+}
+
+PtintArray(arr);
+SelectionSort(arr);
+PtintArray(arr);
 
